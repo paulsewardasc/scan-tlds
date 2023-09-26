0d557516e40690ed2f9550d66d20a4abece2e1da
Paul Seward <pseward@gmail.com>
2023-09-26 17:00:06 +0100

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # scan-tlds
 Scans TLDs for new domains and then scans those domains
+
+## Perequisites
+
+### Nuclei - https://github.com/projectdiscovery/nuclei
+### Notify - https://github.com/projectdiscovery/notify
+### Subfinder - https://github.com/projectdiscovery/subfinder
+### HTTPX - https://github.com/projectdiscovery/httpx
+### Anew - https://github.com/tomnomnom/anew
+
+Note. The script assumes the go files are in $HOME/go/bin
+
---
+++ getnewdomains.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+FOLDER=scantlds
+NEWDOMAINS=newdomains.txt
+NEWFILE=newhttpxscan.txt
+CURRENTDOMAINS=currentdomains.txt
+CURRENTHTTP=currenthttpxscan.txt
+TLDS=tlds.txt
+#exec 3>&1 4>&2
+#trap 'exec 2>&4 1>&3' 0 1 2 3
+#exec 1>/tmp/log.out 2>&1
+if [[ $1 != *"skip"* ]];then
+	(
+	cd $HOME/$FOLDER
+  # Clear out new domains file
+	> $NEWDOMAINS
+	for i in $(cat $TLDS); do $HOME/go/bin/subfinder -d $i >> $NEWDOMAINS; done
+	#cat $NEWDOMAINS | $HOME/go/bin/anew $CURRENTDOMAINS | $HOME/go/bin/notify -bulk  | $HOME/go/bin/nuclei -rl 10 -c 3 -etags dns -exclude-severity info -severity high --stats -t http/cves | $HOME/go/bin/notify -bulk
+	cat $NEWDOMAINS | $HOME/go/bin/anew $CURRENTDOMAINS | $HOME/go/bin/notify -bulk 
+	) 2>&1 | tee /tmp/output.log
+fi
+# Find new http(s) sites - excelure CloudFlare
+cat $CURRENTDOMAINS | httpx -fr -sc -ct -title -fs Cloudflare -o $NEWFILE
+# Add them to the existing list and notify
+cat $NEWFILE | $HOME/go/bin/anew $CURRENTHTTP | $HOME/go/bin/notify -bulk
+# Scan the new sites with nuclei to check for open S3 bucket
+cat $NEWFILE | awk '{print $1}'  | nuclei -dr -t $HOME/custom-nuclei-templates/aws-s3-open-bucket-2.yaml | $HOME/go/bin/notify -bulk
+
