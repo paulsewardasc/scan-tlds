6aedb003e851eeeddea379eac51e16c573744db1
Paul Seward <pseward@gmail.com>
2023-10-10 13:34:46 +0100

    updated the tld scan

---
+++ scantld.sh
@@ -0,0 +1,8 @@
+### This program getnexttld.py looks for a file called tlds.txt with Top Level Domains in and saves it's place in a file called tlds.ind ###
+
+### You can the run this using a cron to scan a TLD every hour or day depending on the number of TLDs you have
+
+### Change the line below to point to the folder with the files in
+cd <YOURFOLDER>
+
+python3 .py | subfinder | nuclei -es info -t http -rl 50 -c 10
