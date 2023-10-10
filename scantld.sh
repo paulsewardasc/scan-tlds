a4ccb2027e19bc9a1edb10897b4894b38feae503
Paul Seward <pseward@gmail.com>
2023-10-10 13:59:48 +0100

    made scantlds.sh executable

--- scantld.sh
+++ scantld.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/bash
+
 ### This program getnexttld.py looks for a file called tlds.txt with Top Level Domains in and saves it's place in a file called tlds.ind ###
 
 ### You can the run this using a cron to scan a TLD every hour or day depending on the number of TLDs you have
