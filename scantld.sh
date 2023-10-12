5339ccf876224a55059eeee6e93c199ce3a7485b
Paul Seward <pseward@gmail.com>
2023-10-12 10:52:23 +0100

    Checking sites found with issues

--- scantld.sh
+++ scantld.sh
@@ -15,10 +15,11 @@ OUTPUT=$(mktemp output-XXXXXX)
 SUBS=$(mktemp subs-XXXXXX)
 python3 getnexttld.py | subfinder -o $SUBS
 cat $SUBS | nuclei -es info -t http -rl 50 -c 10  -H "X-Forwarded-For: 10.255.255.254" -silent -o $OUTPUT
-cat $OUTPUT
 if [[ $(wc -l < output.txt) -ge 1 ]]; then
-  echo "Full scan"
-  #nuclei -rl 50 -c 10 -H "X-Forwarded-For: 10.255.255.255" | notify -bulk
+  SITES=$(cat $OUTPUT | perl -pe "s{.*?https?://(.*?)/.*}{\1}" | sort -u)
+  for i in $SITES; do
+    echo $i | nuclei -rl 50 -c 10 -H "X-Forwarded-For: 10.255.255.255" | notify -bulk
+  done
 fi
 rm $OUTPUT
 rm $SUBS
