a06eeefb8a11174ac2a58195fbe0c271260252b7
Paul Seward <pseward@gmail.com>
2023-10-12 12:45:48 +0100

    Typo

--- scantld.sh
+++ scantld.sh
@@ -15,7 +15,7 @@ OUTPUT=$(mktemp output-XXXXXX)
 SUBS=$(mktemp subs-XXXXXX)
 python3 getnexttld.py $1 | subfinder -o $SUBS
 cat $SUBS | nuclei -es info -t http -rl 50 -c 10  -H "X-Forwarded-For: 10.255.255.254" -silent -o $OUTPUT
-if [[ $(wc -l < output.txt) -ge 1 ]]; then
+if [[ $(wc -l < $OUTPUT) -ge 1 ]]; then
   SITES=$(cat $OUTPUT | perl -pe "s{.*?https?://(.*?)/.*}{\1}" | sort -u)
   for i in $SITES; do
     echo $i | nuclei -rl 50 -c 10 -H "X-Forwarded-For: 10.255.255.255" | notify -bulk
