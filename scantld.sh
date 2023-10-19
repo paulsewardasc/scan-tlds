541dd7c48adc23dd4a2b8e4f5cd61d664b0599c9
Paul Seward <pseward@gmail.com>
2023-10-19 08:33:03 +0100

    Added file for alerting

--- scantld.sh
+++ scantld.sh
@@ -35,7 +35,7 @@ cat $SUBS | nuclei -es info -rl 50 -c 10 -H "X-Forwarded-For: 10.255.255.254" -t
 if [[ $(wc -l < $OUTPUT) -ge 1 ]]; then
   SLACK_API_TOKEN=$(cat $HOME/.config/notify/provider-config.yaml | grep "slack_webhook_url" | head --lines=1 | awk '{print $2}' | perl -pe "s{\"}{}g;s{.*services/(.*)}{\1}")
   export SLACK_API_TOKEN
-  ./send_to_slack
+  ./send_to_slack $OUTPUT
   cat $OUTPUT | awk 'BEGIN {print "SCAN Summary\r"} {print $0}' | perl -pe 's{\n}{\r}gsx' | notify -p discord -bulk -cl 10000
   DTE=$(date +%Y%m%d%H%M%S)
   cat $OUTPUT > results/$DTE.txt
